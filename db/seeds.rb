9312fb2339c1203dd70f79a3ee91c1706eaa6546
Nicholas Utikal <nicholasutikal@gmail.com>
2018-02-16 18:16:08 +0100

    fixed pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Cocktail.destroy_all
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "sugar")
 Ingredient.create(name: "tonic water")
